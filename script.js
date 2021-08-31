b1238870875f184a057a13b8044254bdaf06664f
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-08-31 08:00:23 +0200

    trying the `lookAt` function

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 0.7;
+mesh.position.y = -0.6;
+mesh.position.z = 1;
 scene.add(mesh);
 
 //sizes
-const sizes = { width: window.innerWidth, height:window.innerHeight };
+const sizes = { width: window.innerWidth, height: window.innerHeight };
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 2;
-camera.position.x = 0;
-camera.position.y = 0;
+camera.position.z = 3;
+camera.lookAt(mesh.position)
 scene.add(camera);
-
 //renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
