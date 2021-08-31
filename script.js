0cbccf1373a2e0148625a2f8513bc595625da9c0
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-08-31 08:12:10 +0200

    Groups

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const scene = new THREE.Scene();
 //red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
-scene.add(mesh);
-
+const group = new THREE.Group();
+scene.add(group);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+group.scale.set (2,3,2) 
+group.rotation.y = 1;
 //sizes
 const sizes = { width: window.innerWidth, height: window.innerHeight };
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-camera.lookAt(mesh.position)
+camera.position.z = 7;
+camera.lookAt(cube3.position)
 scene.add(camera);
 //renderer
 const canvas = document.querySelector(".webgl");
